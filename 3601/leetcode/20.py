4116b425aaa9b5e82e062be00c9d3ae575fa3139
王栋梁 <246081156@qq.com>
2022-12-03 16:35:01 +0800

    update

--- 3601/leetcode/20.py
+++ 3601/leetcode/20.py
@@ -1,3 +1,4 @@
+#https://blog.csdn.net/zhizhengguan/article/details/126750993?spm=1001.2101.3001.6650.3&utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7ECTRLIST%7ERate-3-126750993-blog-127418027.pc_relevant_default&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7ECTRLIST%7ERate-3-126750993-blog-127418027.pc_relevant_default&utm_relevant_index=6
 def isValid(s) :
     if len(s) % 2 == 1:
         return False
